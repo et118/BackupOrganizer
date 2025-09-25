82b2b708863bc029893f297f845606013d27afed
et118 <pekdator@gmail.com>
2025-09-25 14:08:27 +0200

    Added DataCollection storage class

---
+++ DataCollection.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+from BackupEntry import BackupEntry
+
+class DataCollection:
+    """Storage class for BackupEntries
+
+    Attributes:
+        name: The name of the DataCollection
+        description: The description of the DataCollection
+        creation_date: The date this DataCollection was created
+        last_modified_date: The date this Datacollection was created. (default creation_date)
+        still_updated: Flag indicating if this DataCollection is up to date
+        backup_entries: List containing all BackupEntries
+    """
+    def __init__(self, name : str, description : str) -> None:
+        """Creates a new DataCollection with the last_modified_date set to creation_date.
+        
+        Args:
+            name: the name of the DataCollection.
+            description: the description of the DataCollection.
+        """
+        current_date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+        self.name: str = name
+        self.description: str = description
+        self.creation_date: str = current_date
+        self.last_modified_date: str = current_date
+        self.still_updated: bool = True
+        self.backup_entries: list[BackupEntry] = []
