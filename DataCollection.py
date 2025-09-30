2e2f2c9c22fd80f1c9ea8b3722b4d962be403216
et118 <pekdator@gmail.com>
2025-09-30 11:59:59 +0200

    Added brief_str and full_str methods + documentation to DataCollection class

--- DataCollection.py
+++ DataCollection.py
@@ -1,4 +1,3 @@
-from Utility import get_current_datestring
 from BackupEntry import BackupEntry
 
 class DataCollection:
@@ -8,22 +7,56 @@ class DataCollection:
         name: The name of the DataCollection
         description: The description of the DataCollection
         creation_date: The date this DataCollection was created
-        last_modified_date: The date this Datacollection was created. (default creation_date)
-        still_updated: Flag indicating if this DataCollection is up to date
+        modification_date: The date this Datacollection was created. (default creation_date)
+        updated: Flag indicating if this DataCollection is up to date
         backup_entries: List containing all BackupEntries
     """
-    def __init__(self, name : str, description : str) -> None:
-        """Creates a new DataCollection with the last_modified_date set to creation_date.
+    def __init__(self, name : str, description : str, creation_date : str, modification_date : str, updated : bool) -> None:
+        """Creates a new DataCollection with the modification_date set to creation_date.
         
         Args:
-            name: the name of the DataCollection.
-            description: the description of the DataCollection.
+            name: The name of the DataCollection.
+            description: The description of the DataCollection.
+            creation_date: The datestring containing the date of creation
+            modification_daate: The datestring containing the date of modification
+            updated: If the DataCollection is updated
         """
-        current_date = get_current_datestring()
 
         self.name: str = name
         self.description: str = description
-        self.creation_date: str = current_date
-        self.last_modified_date: str = current_date
-        self.still_updated: bool = True
+        self.creation_date: str = creation_date
+        self.modification_date: str = modification_date
+        self.updated: bool = updated
         self.backup_entries: list[BackupEntry] = []
+
+    def brief_str(self) -> str:
+        """Returns a string containing the following attributes as strings:
+        * name
+        * modification_date
+        * updated
+
+        Example Return Value:
+            "{name} | {modification_date} | Updated: {updated}"
+        """
+        string = ""
+        string += self.name + " | "
+        string += self.modification_date + " | "
+        string += "Updated: " + str(self.updated) + "\n"
+        return string
+
+    def full_str(self) -> list[str]:
+        """Returns a list containing the following attributes as strings:
+        * name
+        * description
+        * creation_date
+        * modification_date
+        * updated
+        """
+        string_list = []
+        string_list.append(self.name)
+        string_list.append(self.description)
+        string_list.append(self.creation_date)
+        string_list.append(self.modification_date)
+        string_list.append(str(self.updated))
+
+        return string_list
