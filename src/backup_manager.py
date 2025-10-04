bc8e80f55b05340e67b388cacf62f99d78081444
et118 <pekdator@gmail.com>
2025-10-04 17:55:05 +0200

    Added archived function for adding BackupEntries

--- src/backup_manager.py
+++ src/backup_manager.py
@@ -1,4 +1,6 @@
 
+from data_collection import DataCollection
+
 class BackupManager():
     """Unused Class being left for Archival purposes. 
     
@@ -6,6 +8,17 @@ class BackupManager():
     but added unecessary abstraction. All functionality has been moved to 
     the DataCollection object
 
-    DataCollection.add_backup()
-    DataCollection.unbackup()
+    BackupManager.add_backup() -> DataCollection.add_backup()
     """
+    def add_backup(self, collection_object : DataCollection, backup_name : str, backup_date : str, backup_location : str) -> None:
+        """ARCHIVED! This method is no longer in use.
+        
+        Adds a BackupEntry to the `collection_object`
+
+        Args:
+            `collection_object`: The DataCollection we want to add the new BackupEntry to
+            `backup_name`: The name of the new BackupEntry
+            `backup_date`: The date of the BackupEntry as a string
+            `backup_location`: The path where the backup is stored
+        """
+        collection_object.add_backup(backup_name, backup_date, backup_location)
