051d17acb6388ef0641aeaa10c7205093f0271bf
et118 <pekdator@gmail.com>
2025-10-05 18:22:18 +0200

    Added test for Utility.get_current_datestring()

---
+++ tests/unit/test_utility.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+import pytest
+from src.utility import get_current_datestring
+
+class MockDatetime:
+    #I'm really not sure what this decorator does. Something about
+    #being a reference to create a new class. When i look at
+    #datetime.datetime.now() function, it uses it, so i use it here too.
+    @classmethod 
+    def now(cls, tz=None):
+        return datetime(2025,6,5,23,49,00)
+
+def test_utility_get_current_datestring(monkeypatch):
+    expected = "2025-06-05 23:49:00"
+    
+    monkeypatch.setattr("src.utility.datetime", MockDatetime)
+    output = get_current_datestring()
+
+    assert expected == output
