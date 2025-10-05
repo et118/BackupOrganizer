a874355d2b343574da1d7ac7c5145f96e58ce66e
et118 <pekdator@gmail.com>
2025-10-05 17:48:17 +0200

    Added new CollectionAlreadyExists exception

--- src/custom_exceptions.py
+++ src/custom_exceptions.py
@@ -1,5 +1,8 @@
 class CollectionNotFoundError(Exception):
     """Raised when no DataCollection matching the criteria was found"""
 
+class CollectionAlreadyExists(Exception):
+    """Raised when trying to add a collection with the same name as another"""
+
 class BackupNotFoundError(Exception):
     """Raised when no BackupEntry matching the criteria was found"""
