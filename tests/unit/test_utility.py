0d84b4a86e7725bccf4a37169d21cbaaab1b75ab
et118 <pekdator@gmail.com>
2025-10-08 13:17:42 +0200

    Added clarification to test using @classmethod

--- tests/unit/test_utility.py
+++ tests/unit/test_utility.py
@@ -3,8 +3,9 @@ from src.utility import get_current_datestring
 
 class MockDatetime:
     #I'm really not sure what this decorator does. Something about
-    #being a reference to create a new class. When i look at
-    #datetime.datetime.now() function, it uses it, so i use it here too.
+    #being a reference to create a new class. When I look at
+    #datetime.datetime.now() function, it uses it, so i use it here too
+    #to make the function structure the same as the one I'm overriding.
     @classmethod 
     def now(cls, tz=None):
         return datetime(2025,6,5,23,49,00)
